23adda4ed3b142929622f888d0e03beecb003951
ikx94 <katzian@gmail.com>
2019-04-29 04:13:21 -0300

    Reset

--- dota2draft.js
+++ dota2draft.js
@@ -42,7 +42,7 @@ app.disable('view layout');
 
 var server = http.createServer(app);
 var io = require('socket.io').listen(server);
-server.listen(80);
+server.listen(9000);
 
 //////////////////////
 // Routing
